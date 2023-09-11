545282578e6f17386fa55212e8b6b4a5f5f6b2e0
ahmadhassoun366 <ahmadhss366@gmail.com>
2023-09-11 14:35:20 -0700

    debug

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -55,12 +55,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'projectname.urls'
@@ -139,6 +139,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ALLOWED_HOSTS=['*']
 CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS=['*']
+
